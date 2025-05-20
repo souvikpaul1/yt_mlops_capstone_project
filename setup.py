026bc63b7a91262ffb4f3487a691c4663ae647d3
souvikpaul1 <souvikpaul425@gmail.com>
2025-05-20 21:05:50 +0530

    project structure ready

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is e2e ML ops project',
+    author='Souvik Paul',
+    license='',
+)
